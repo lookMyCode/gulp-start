4fa54fa02544d1265b9815548d59603e6acb043b
Dmytro <dimasik22101993@gmail.com>
2022-04-29 13:04:11 +0200

    removed spaces

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,14 @@ const path = {
     html: [`${SOURSE_FOLDER}/*.html`, `!${SOURSE_FOLDER}/_*.html`],
     css: `${SOURSE_FOLDER}/scss/style.scss`,
     js: `${SOURSE_FOLDER}/js/script.js`,
-    img: `${SOURSE_FOLDER}/img/**/*.{jpg, png, svg, gif, ico, webp}`,
+    img: `${SOURSE_FOLDER}/img/**/*.{jpg,png,svg,gif,ico,webp}`,
     fonts: `${SOURSE_FOLDER}/fonts/*.ttf`,
   },
   watch: {
     html: `${SOURSE_FOLDER}/**/*.html`,
     css: `${SOURSE_FOLDER}/scss/**/*.scss`,
     js: `${SOURSE_FOLDER}/js/**/*.js`,
-    img: `${SOURSE_FOLDER}/img/**/*.{jpg, png, svg, gif, ico, webp}`,
+    img: `${SOURSE_FOLDER}/img/**/*.{jpg,png,svg,gif,ico,webp}`,
   },
   clean: `./${PROJECT_FOLDER}/`,
 }
